1a13b1f1705cfd4e8e3ff17000268c376fbf8f6e
flaskur <flaskur@gmail.com>
2020-09-13 22:09:01 -0400

    implemented and test image normalize function

--- Project1/main.m
+++ Project1/main.m
@@ -6,7 +6,7 @@ load './Project1DataFiles/cifar10testdata.mat'
 % This iterates through each picture class (ie airplane, automobile, etc.)
 % and access a single image that is a 3 dimensional matrix (row, column,
 % color channel).
-for classindex = 1:2
+for classindex = 1
     % indices represents a vector/array of 1000 elements -> row of 1000
     % elements
     indices = find(trueclass==classindex);
@@ -19,6 +19,10 @@ for classindex = 1:2
     size(image_rgb) % 3D matrix [32 x 32 x 3]
     
     disp(image_rgb) % will print the entire 3d matrix
+    
+    % testing that apply_imnormalize() worked
+    inarray = image_rgb;
+    outarray = apply_imnormalize(inarray); % should invoke disp(image) as well
     
     % Shows the image.
     figure(1); colormap(gray); imagesc(image_rgb); truesize(gcf, [64, 64]);
