e3c91ad103d5d474f110044c99521530f7dfd4fc
ledzeppelin1201 <60328435+ledzeppelin1201@users.noreply.github.com>
2020-11-07 12:20:21 -0500

    Update functions

--- Project2/findEpipolarLines.m
+++ Project2/findEpipolarLines.m
@@ -1,6 +1,7 @@
 function [EpipolarLines1, EpipolarLines2] = findEpipolarLines(worldCoord3DPoints, cam1, ...
     cam1PixelCoords, cam2, cam2PixelCoords)
     
+    % Determine the location of cam2 seen by cam1
     % Pad the Pmat(3*4) to 4*4 matrix with 0, 1 for multiplication
     Pmat1 = padarray(cam1.Pmat,1,0,'post');  Pmat1(4,4)=1;
     % Rotate and translate WorldCord into camera coordinates
@@ -8,17 +9,16 @@ function [EpipolarLines1, EpipolarLines2] = findEpipolarLines(worldCoord3DPoints
     % Apply forward projection, translate into pixel coordinates
     FilmCord1 = [cam1.foclen 0 0 0; 0 cam1.foclen 0 0; 0 0 1 0]*CamCord1;
     FilmCord1x=FilmCord1(1)/FilmCord1(3); FilmCord1y=FilmCord1(2)/FilmCord1(3); % Perspective projection
-    u1=FilmCord1x+960; v1=FilmCord1y+540; % Adding half of size of pixels, Epipole1
-    Epipole1 = [u1,v1];
-    
+    u1=FilmCord1x+960; v1=FilmCord1y+540; % Adding half of size of pixels
+    Epipole1 = [u1,v1]; % Acquired the epipole
     % Solve linear equation using the joint point and the epipole
     syms a1 b1
-    eqn1(1) = a1*u1+b1*v1+1==0;
+    eqn1(1) = a1*Epipole1(1)+b1*Epipole1(2)+1==0;
     eqn1(2) = a1*cam1PixelCoords(1)+b1*cam1PixelCoords(2)+1==0;
     Sol1 = solve(eqn1,[a1 b1]);
     EpipolarLines1 = double([Sol1.a1,Sol1.b1,1]');
     
-    
+    % Determine the location of cam1 seen by cam2
     % Pad the Pmat(3*4) to 4*4 matrix with 0, 1 for multiplication
     Pmat2 = padarray(cam2.Pmat,1,0,'post');  Pmat2(4,4)=1;
     % Rotate and translate WorldCord into camera coordinates
@@ -26,43 +26,14 @@ function [EpipolarLines1, EpipolarLines2] = findEpipolarLines(worldCoord3DPoints
     % Apply forward projection, translate into pixel coordinates
     FilmCord2 = [cam2.foclen 0 0 0; 0 cam2.foclen 0 0; 0 0 1 0]*CamCord2;
     FilmCord2x=FilmCord2(1)/FilmCord2(3); FilmCord2y=FilmCord2(2)/FilmCord2(3); % Perspective projection
-    u2=FilmCord2x+960; v2=FilmCord2y+540; % Adding half of size of pixels, Epipole2
-    Epipole2 = [u2,v2];
-    
+    u2=FilmCord2x+960; v2=FilmCord2y+540; % Adding half of size of pixels
+    Epipole2 = [u2,v2]; % Acquired the epipole
     % Solve linear equation using the joint point and the epipole
     syms a2 b2
-    eqn2(1) = a2*u2+b2*v2+1==0;
+    eqn2(1) = a2*Epipole2(1)+b2*Epipole2(1)+1==0;
     eqn2(2) = a2*cam2PixelCoords(1)+b2*cam2PixelCoords(2)+1==0;
     Sol2 = solve(eqn2,[a2 b2]);
     EpipolarLines2 = double([Sol2.a2,Sol2.b2,1]');
-    
-        %{
-    syms f11 f12 f13 f21 f22 f23 f31 f32 f33
-    F_var = [f11 f12 f13; f21 f22 f23; f31 f32 f33];
-    
-    for i=1:12
-        multF(i) = cam1PixelCoords(:,i)'*F_var*cam2PixelCoords(:,i);
-        eqn(i) = multF(i)==0;
-    end
-    
-    Sol = solve(eqn,[f11 f12 f13 f21 f22 f23 f31 f32 f33]);
-    
-    F = double([Sol.f11 Sol.f12 Sol.f13; 
-                Sol.f21 Sol.f22 Sol.f23; 
-                Sol.f31 Sol.f32 Sol.f33]);
-
-    % Derive both vectors(rays) from inputs
-    V1 = R1'*inv(K1)*Pu1;
-    uV1 = V1/sum(abs(V1));
- 
-    V2 = R2'*inv(K2)*Pu2;
-    uV2 = V2/sum(abs(V2));
-
-    C1 = cam1.position'; C2 = cam2.position';
-    T = abs(C2-C1);
-    Tx=T(1); Ty=T(2); Tz=T(3);
-    S = [0 -Tz Ty; Tz 0 -Tx; -Ty Tx 0];
-    %}
     
     
 end
--- Project2/reconstruct3DFrom2D.m
+++ Project2/reconstruct3DFrom2D.m
@@ -3,14 +3,16 @@ function recovered3DPoints = reconstruct3DFrom2D(cam1, cam1PixelCoords, cam2, ca
     Pmat1 = padarray(cam1.Pmat,1,0,'post');  Pmat1(4,4)=1;
     Pmat2 = padarray(cam2.Pmat,1,0,'post');  Pmat2(4,4)=1;
     
-    Pu1 = cam1PixelCoords; Pu2 = cam2PixelCoords;
+    % Determine matrices Pu, K, C, R for triangulation
+    Pu1 = cam1PixelCoords; Pu2 = cam2PixelCoords; % Input two 2D points
     
+    % 
     K1 = [1 0 960; 0 1 540; 0 0 1]*[cam1.foclen 0 0 ; 0 cam1.foclen 0 ; 0 0 1 ];
     K2 = [1 0 960; 0 1 540; 0 0 1]*[cam2.foclen 0 0 ; 0 cam2.foclen 0 ; 0 0 1 ];
     
-    C1 = cam1.position'; C2 = cam2.position';
+    C1 = cam1.position'; C2 = cam2.position'; % 3*1 camera coordinates
     
-    R1 = Pmat1(1:3,1:3); R2 = Pmat2(1:3,1:3);
+    R1 = Pmat1(1:3,1:3); R2 = Pmat2(1:3,1:3); % 3*3 rotation matrices
     
     % Derive both vectors(rays) from inputs
     V1 = R1'*inv(K1)*Pu1;
@@ -27,6 +29,7 @@ function recovered3DPoints = reconstruct3DFrom2D(cam1, cam1PixelCoords, cam2, ca
     E1 = a*uV1-b*uV2+d*uV3;  E2=C2-C1;
     eqn(1) = E1(1) == E2(1); eqn(2) = E1(2) == E2(2); eqn(3) = E1(3) == E2(3);
     S = solve(eqn,[a b d]);
+    
     % Using the unit vectors, scale factors and camera cords to compute Pw
     P1 = double(S.a)*uV1+C1; P2 = double(S.b)*uV2+C2;
     recovered3DPoints = (P1+P2)/2;  %recovered3DPoints(Pw)
