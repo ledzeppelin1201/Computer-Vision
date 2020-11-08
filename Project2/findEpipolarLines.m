7c008017be055127acbebc633bfe6a9b47bbe977
ledzeppelin1201 <60328435+ledzeppelin1201@users.noreply.github.com>
2020-11-07 19:50:19 -0500

    Update findEpipolarLines.m

--- Project2/findEpipolarLines.m
+++ Project2/findEpipolarLines.m
@@ -4,7 +4,7 @@ function [EpipolarLines1, EpipolarLines2] = findEpipolarLines(worldCoord3DPoints
     % Determine the location of cam2 seen by cam1
     % Pad the Pmat(3*4) to 4*4 matrix with 0, 1 for multiplication
     Pmat1 = padarray(cam1.Pmat,1,0,'post');  Pmat1(4,4)=1;
-    % Rotate and translate WorldCord into camera coordinates
+    % Rotate and translate WorldCord of camera2 position into camera1 coordinates
     CamCord1 = Pmat1*[cam2.position 1]';
     % Apply forward projection, translate into pixel coordinates
     FilmCord1 = [cam1.foclen 0 0 0; 0 cam1.foclen 0 0; 0 0 1 0]*CamCord1;
@@ -21,7 +21,7 @@ function [EpipolarLines1, EpipolarLines2] = findEpipolarLines(worldCoord3DPoints
     % Determine the location of cam1 seen by cam2
     % Pad the Pmat(3*4) to 4*4 matrix with 0, 1 for multiplication
     Pmat2 = padarray(cam2.Pmat,1,0,'post');  Pmat2(4,4)=1;
-    % Rotate and translate WorldCord into camera coordinates
+    % Rotate and translate WorldCord of camera1 position into camera2 coordinates
     CamCord2 = Pmat2*[cam1.position 1]';
     % Apply forward projection, translate into pixel coordinates
     FilmCord2 = [cam2.foclen 0 0 0; 0 cam2.foclen 0 0; 0 0 1 0]*CamCord2;
